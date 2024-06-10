917bb51ffdc6bd60847548d244c53096ebd2c2e5
furqan6981 <fa517773@gmail.com>
2024-06-10 22:26:22 +0500

    Uploaded Task 01

---
+++ Task-01/app.js
@@ -0,0 +1,13 @@
+let navLinks = document.getElementById('navLinks');
+
+function showMenu() {
+    navLinks.style.display = 'block';
+
+}
+function hideMenu() {
+    navLinks.style.display = '';
+}
+
+function down() {
+    location.href="c:\Users\x250\Downloads\Furqans-Resume--.pdf";
+}
